41a215453c0edc1bb815c8af9084ffd0a000d47d
Monyett <monyett.tanzillo@gmail.com>
2016-01-24 19:02:59 -0500

    Complete checkpoint 22 searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -29,7 +29,26 @@ def add_entry(name, phone_number, email)
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+   end
 
+   def binary_search(name)
+     lower = 0
+     upper = @entries.length - 1
 
- end
+     while lower <= upper
+       mid = (lower + upper) / 2
+       mid_name = @entries[mid].name
+
+       if name == mid_name
+         return @entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     return nil
+   end
+
 end
